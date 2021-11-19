c2994b07e1fdc882b1044adf84b555f904026956
Oriana1311 <orianagildelgado@gmail.com>
2021-11-19 17:03:01 +0100

    CSS general

--- cabecera.php
+++ cabecera.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Entrada</title>
+    <title>proyectoPHP</title>
     <link rel="stylesheet" href="styles/cabecera.css">
-    <link rel="stylesheet" href="styles/registro.css">
+    <link rel="stylesheet" href="styles/general.css">
     
 </head>
 <body>
